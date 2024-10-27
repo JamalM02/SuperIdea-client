a2dc0078d238759ef4782ecddbcd35265d284ae2
Jamal Majadle <jamal.majadle02@students.kinneret.ac.il>
2024-10-27 15:24:44 +0200

    integrate Vercel Analytics

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { Analytics } from '@vercel/analytics/react';
+
 
 const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
@@ -15,6 +17,7 @@ root.render(
         <GoogleOAuthProvider clientId={clientId}>
             <Router>
                 <App />
+                <Analytics />
             </Router>
         </GoogleOAuthProvider>
     </React.StrictMode>
